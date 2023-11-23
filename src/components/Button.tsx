06fe47366e91be19be1ef1da9b5cf09ef49661d7
Maik Emanoel <maikemanoel11@gmail.com>
2023-11-23 10:40:47 -0300

    handle with tooptip animation & import isTouchSupported

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,42 @@
 import { ButtonHTMLAttributes, ReactNode, useState } from "react";
+import { isTouchSupported } from "../utils/touchUtils";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  tooltipName: string
+  tooltipName: string;
 }
 
-export function Button({ children, tooltipName,  ...rest }: ButtonProps) {
-  const [tooltipIsVisible, setTooltipIsVisible] = useState(false);
+type Tooltip = "visible" | "invisible" | "animationLeave";
+
+export function Button({ children, tooltipName, ...rest }: ButtonProps) {
+  const [tooltipVisibility, setTooltipVisibility] = useState<Tooltip>("invisible");
 
   return (
     <div
       className="relative text-white"
-      onMouseEnter={() => setTooltipIsVisible(true)}
-      onMouseLeave={() => setTooltipIsVisible(false)}
+      onMouseEnter={() => setTooltipVisibility("visible")}
+      onMouseLeave={() => {
+        setTooltipVisibility("animationLeave");
+
+        setTimeout(() => {
+          setTooltipVisibility("invisible");
+        }, 300);
+      }}
     >
       <button
-        className="w-9 h-9 grid place-items-center rounded-full bg-purple active:scale-90"
+        data-istouchsupported={isTouchSupported}
+        className="w-9 h-9 grid place-items-center rounded-full bg-purple transition-all duration-300 active:scale-90 data-[istouchsupported=false]:hover:brightness-110"
         {...rest}
       >
         {children}
       </button>
 
-      {tooltipIsVisible && (
+      {tooltipVisibility !== "invisible" && !isTouchSupported && (
         <div
           id="tooltip-default"
           role="tooltip"
-          className="absolute left-[50%] translate-x-[-50%] mt-2 px-3 py-1 rounded-lg bg-gray/75 backdrop-blur text-sm"
+          data-tooltipvisibility={tooltipVisibility}
+          className="absolute left-[50%] translate-x-[-50%] mt-2 px-3 py-1 rounded-lg bg-gray/75 backdrop-blur text-sm select-none pointer-events-none data-[tooltipvisibility=visible]:animate-appear data-[tooltipvisibility=animationLeave]:animate-disappear"
         >
           <span>{tooltipName}</span>
         </div>
