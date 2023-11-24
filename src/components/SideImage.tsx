22fdfc11bca514f12105d6f4dd08cbc401c8ea2d
Maik Emanoel <maikemanoel11@gmail.com>
2023-11-24 11:24:52 -0300

    remove sideImage in small screens

--- src/components/SideImage.tsx
+++ src/components/SideImage.tsx
@@ -2,7 +2,7 @@ import rocket from "../assets/rocket.svg";
 
 export function SideImage() {
   return (
-    <div className="max-w-[400px]">
+    <div className="max-w-[400px] md:hidden">
       <img src={rocket} alt="Imagem de plano de fundo de um foguete" />
     </div>
   );
