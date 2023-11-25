989d164923dba2b52a296695c2b19c9b495b48a6
Maik Emanoel <maikemanoel11@gmail.com>
2023-11-25 15:35:29 -0300

    handle with vanilla tilt lib

--- src/components/SideImage.tsx
+++ src/components/SideImage.tsx
@@ -1,9 +1,37 @@
-import rocket from "../assets/rocket.svg";
+import { useEffect, useRef } from "react";
+import spaceBackground from "../assets/space-background.svg";
+import rocket from "../assets/rocket.png";
+import VanillaTilt from "vanilla-tilt";
 
 export function SideImage() {
+  const imageRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (imageRef.current) {
+      VanillaTilt.init(imageRef.current, {
+        max: 20,
+        speed: 400,
+      });
+    }
+  }, []);
+
   return (
-    <div className="max-w-[400px] md:hidden">
-      <img src={rocket} alt="Imagem de plano de fundo de um foguete" />
+    <div
+      className="max-w-[400px] md:hidden relative"
+      ref={imageRef}
+      style={{ transformStyle: "preserve-3d" }}
+    >
+      <img
+        src={spaceBackground}
+        alt="Imagem de plano de fundo do espaço"
+        style={{ transform: "perspective(500px)" }}
+      />
+      <img
+        src={rocket}
+        alt="Imagem de um foguete em 2D"
+        className="absolute h-[85%] top-0 left-[32%]"
+        style={{ transform: "translateZ(50px)" }}
+      />
     </div>
   );
 }
