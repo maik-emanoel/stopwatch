5298640745bc370f8b4973fa4faf00e7eb5fea9f
Maik Emanoel <maikemanoel11@gmail.com>
2023-11-28 11:11:12 -0300

    change layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import { SideImage } from "./components/SideImage";
 import { Timer } from "./components/Timer";
+import { Splits } from "./components/Splits";
 
 export function App() {
+  const [counter, setCounter] = useState(0);
+  const [splits, setSplits] = useState<number[]>([]);
+
   return (
     <main className="flex justify-center items-center gap-16 max-w-[900px] w-full px-5">
-      <Timer />
-      <SideImage />
+      <Timer counter={counter} setCounter={setCounter} setSplits={setSplits} />
+      <div className="flex flex-col max-w-[400px] max-h-[300px] w-full">
+        {splits.length > 0 ? <Splits splits={splits} /> : <SideImage />}
+      </div>
     </main>
-  )
+  );
 }
