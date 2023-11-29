c652a04d2b0e61f9fac14d053b4a6500e1db2aaa
Maik Emanoel <maikemanoel11@gmail.com>
2023-11-29 09:29:38 -0300

    handle with lastSplitRef & add more styles

--- src/components/Splits.tsx
+++ src/components/Splits.tsx
@@ -1,9 +1,18 @@
+import { useEffect, useRef } from "react";
+
 interface SplitsProps {
   splits: number[];
 }
 
 export function Splits(props: SplitsProps) {
   const { splits } = props;
+  const lastSplitRef = useRef<null | HTMLTableRowElement>(null);
+
+  useEffect(() => {
+    if (lastSplitRef.current) {
+      lastSplitRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  });
 
   const splitsFormatted = splits.map((split) => {
     const minutes = Math.floor(split / (1000 * 60));
@@ -12,25 +21,34 @@ export function Splits(props: SplitsProps) {
 
     return `${minutes.toString().padEnd(2, "0")}:${seconds
       .toString()
-      .padStart(2, "0")}:${milliseconds.toString().padStart(3, "0")}`;
+      .padStart(2, "0")}.${milliseconds.toString().padStart(3, "0")}`;
   });
 
   return (
-    <div className="w-full border border-gray overflow-y-auto rounded-lg backdrop-blur-md splitsScroll">
+    <div
+      className="w-full h-full border border-zinc-300 overflow-y-auto rounded-lg backdrop-blur-md splitsScroll"
+      style={{
+        boxShadow: `10px 9px 40px -25px #DDDBF2`,
+      }}
+    >
       <table className="w-full flex flex-col">
         <thead className="sticky top-0">
-          <tr className="bg-white w-full flex">
-            <th className="py-1 block w-3/4">Lap</th>
-            <th className="py-1 block w-full">Time</th>
+          <tr className="bg-white w-full flex py-2 border-b border-zinc-300 text-zinc-700">
+            <th className="block w-3/4">Lap</th>
+            <th className="block w-full">Time</th>
           </tr>
         </thead>
 
-        <tbody>
+        <tbody className="text-gray">
           {splitsFormatted.map((split, index) => {
             return (
-              <tr className="w-full flex text-center">
+              <tr
+                className="w-full flex text-center"
+                key={index + 1}
+                ref={index === splits.length - 1 ? lastSplitRef : null}
+              >
                 <td className="p-1 block w-3/4">{index + 1}</td>
-                <td className="p-1 block w-full">{split}</td>
+                <td className="p-1 block w-full tracking-wide">{split}</td>
               </tr>
             );
           })}
