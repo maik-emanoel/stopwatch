5a8c65212e5e4b9eb75d826fcc19cc894977c32e
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-01 09:26:28 -0300

    handle with flip effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SideImage } from "./components/SideImage";
 import { Timer } from "./components/Timer";
 import { Splits } from "./components/Splits";
+
+import "swiper/css";
+import "swiper/css/effect-flip";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper/core";
+import { EffectFlip } from "swiper/modules";
+
+SwiperCore.use([EffectFlip]);
 
 export function App() {
   const [counter, setCounter] = useState(0);
   const [splits, setSplits] = useState<number[]>([]);
 
+  const [swiper, setSwiper] = useState<SwiperCore | null>(null);
+
+  useEffect(() => {
+    if (splits.length > 0) {
+      swiper?.slideNext();
+    }
+  }, [splits, swiper]);
+
   return (
     <main className="flex justify-center items-center gap-16 max-w-[900px] w-full px-5 md:flex-wrap">
       <Timer counter={counter} setCounter={setCounter} setSplits={setSplits} />
-      <div className="flex flex-col max-w-[400px] max-h-[300px] w-full md:h-[300px]">
-        {splits.length > 0 ? <Splits splits={splits} /> : <SideImage />}
+      <div className="max-w-[400px] w-full relative">
+        <Swiper
+          effect="flip"
+          flipEffect={{
+            limitRotation: true,
+            slideShadows: false,
+          }}
+          className="max-h-[300px] h-full"
+          onInit={(e) => setSwiper(e)}
+          onClick={() => {
+            if (swiper) {
+              swiper.slidePrev();
+            }
+          }}
+        >
+          <SwiperSlide>
+            <SideImage />
+          </SwiperSlide>
+          {splits.length > 0 && (
+            <SwiperSlide>
+              <Splits splits={splits} />
+            </SwiperSlide>
+          )}
+        </Swiper>
       </div>
     </main>
   );
