a4cd01388496cc3a3b45de56ea4d7e39ab5514fa
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-04 10:09:16 -0300

    remove all the toggleTheme logic and put on context

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,33 +1,20 @@
 import sunImage from "../assets/sun.svg";
 import moonImage from "../assets/moon.svg";
-import { useState } from "react";
-
-type Theme = "light" | "dark";
+import { useThemeContext } from "../context/ThemeContext";
 
 export function ThemeSwitch() {
-  const [theme, setTheme] = useState<Theme>("light");
-
-  function handleToggleTheme() {
-    const html = document.querySelector("html");
-    setTheme(theme === "light" ? "dark" : "light");
-
-    if (theme === "light") {
-      html?.classList.add("dark");
-    } else {
-      html?.classList.remove("dark");
-    }
-  }
+  const {theme, handleToggleTheme} = useThemeContext()
 
   return (
     <div className="absolute top-5 right-7 select-none">
       <div
-        className="rounded-full w-14 h-5 relative bg-zinc-200"
+        className="rounded-full w-14 h-5 relative bg-zinc-200 dark:bg-white"
         style={{
           boxShadow: `inset 1px 9px 40px -25px #000000ab`,
         }}
         onClick={handleToggleTheme}
       >
-        <div className="absolute top-[50%] translate-y-[-50%] left-0 grid place-items-center w-8 h-8 rounded-full bg-zinc-700 transition-all duration-200 dark:left-[50%]">
+        <div className="absolute top-[50%] translate-y-[-50%] left-0 grid place-items-center w-8 h-8 rounded-full bg-purple transition-all duration-200 dark:left-[50%]">
           {theme === "light" ? (
             <img src={sunImage} alt="Ícone do sol" className="w-5 h-5" />
           ) : (
