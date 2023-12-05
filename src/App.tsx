dfba5012db5a7e67c8ec9594a3a6587705408429
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-05 10:06:09 -0300

    use particles when theme is dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { SideImage } from "./components/SideImage";
 import { Timer } from "./components/Timer";
 import { Splits } from "./components/Splits";
@@ -9,10 +9,22 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper/core";
 import { EffectFlip } from "swiper/modules";
 import { ThemeSwitch } from "./components/ThemeSwitch";
+import Particles from "react-particles";
+import { Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
+import { optionsParticles } from "./particlesjs/particlesjs-config";
+import { useThemeContext } from "./context/ThemeContext";
 
 SwiperCore.use([EffectFlip]);
 
 export function App() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async () => {}, []);
+
+  const { theme } = useThemeContext();
   const [counter, setCounter] = useState(0);
   const [splits, setSplits] = useState<number[]>([]);
 
@@ -25,35 +37,50 @@ export function App() {
   }, [splits, swiper]);
 
   return (
-    <main className="flex justify-center items-center gap-16 max-w-[900px] w-full px-5 md:flex-wrap">
-      <Timer counter={counter} setCounter={setCounter} setSplits={setSplits} />
-      <div className="max-w-[400px] w-full relative">
-        <Swiper
-          effect="flip"
-          flipEffect={{
-            limitRotation: true,
-            slideShadows: false,
-          }}
-          className="max-h-[300px] h-full"
-          onInit={(e) => setSwiper(e)}
-          onClick={() => {
-            if (swiper) {
-              swiper.slidePrev();
-            }
-          }}
-        >
-          <SwiperSlide>
-            <SideImage />
-          </SwiperSlide>
-          {splits.length > 0 && (
+    <>
+      <main className="flex justify-center items-center gap-16 max-w-[900px] w-full px-5 z-10 md:flex-wrap">
+        <Timer
+          counter={counter}
+          setCounter={setCounter}
+          setSplits={setSplits}
+        />
+        <div className="max-w-[400px] w-full relative">
+          <Swiper
+            effect="flip"
+            flipEffect={{
+              limitRotation: true,
+              slideShadows: false,
+            }}
+            className="max-h-[300px] h-full"
+            onInit={(e) => setSwiper(e)}
+            onClick={() => {
+              if (swiper) {
+                swiper.slidePrev();
+              }
+            }}
+          >
             <SwiperSlide>
-              <Splits splits={splits} />
+              <SideImage />
             </SwiperSlide>
-          )}
-        </Swiper>
-      </div>
+            {splits.length > 0 && (
+              <SwiperSlide>
+                <Splits splits={splits} />
+              </SwiperSlide>
+            )}
+          </Swiper>
+        </div>
 
-      <ThemeSwitch />
-    </main>
+        <ThemeSwitch />
+      </main>
+
+      {theme === "dark" && (
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={optionsParticles}
+        />
+      )}
+    </>
   );
 }
