865563387fd03436723f3f02144abf41fedf2331
Maik Emanoel <maikemanoel11@gmail.com>
2023-12-05 09:19:31 -0300

    add hover on theme button

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,9 +1,10 @@
 import sunImage from "../assets/sun.svg";
 import moonImage from "../assets/moon.svg";
 import { useThemeContext } from "../context/ThemeContext";
+import { isTouchSupported } from "../utils/touchUtils";
 
 export function ThemeSwitch() {
-  const {theme, handleToggleTheme} = useThemeContext()
+  const { theme, handleToggleTheme } = useThemeContext();
 
   return (
     <div className="absolute top-5 right-7 select-none">
@@ -14,13 +15,16 @@ export function ThemeSwitch() {
         }}
         onClick={handleToggleTheme}
       >
-        <div className="absolute top-[50%] translate-y-[-50%] left-0 grid place-items-center w-8 h-8 rounded-full bg-purple transition-all duration-200 dark:left-[50%]">
+        <button
+          data-istouchsupported={isTouchSupported}
+          className="absolute top-[50%] translate-y-[-50%] left-0 grid place-items-center w-8 h-8 rounded-full bg-purple transition-all duration-200 dark:left-[50%] cursor-pointer data-[istouchsupported=false]:hover:outline data-[istouchsupported=false]:hover:outline-8 data-[istouchsupported=false]:hover:outline-black/20 dark:data-[istouchsupported=false]:hover:outline-white/20"
+        >
           {theme === "light" ? (
             <img src={sunImage} alt="Ícone do sol" className="w-5 h-5" />
           ) : (
             <img src={moonImage} alt="Ícone do lua" className="w-5 h-5" />
           )}
-        </div>
+        </button>
       </div>
     </div>
   );
